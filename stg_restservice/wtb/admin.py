d63bded60a0e3b027846575d9ceaf1494d146caa
Владимир Леонтьев <dllreg.exe@gmail.com>
2022-10-08 21:16:48 +0800

    add tables to admin- panel

--- stg_restservice/wtb/admin.py
+++ stg_restservice/wtb/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from wtb.models import (
+    WtbUser, Wallet, Answer, Article, Quiz, Game, GameResult, Like,
+    Publication, Question, Repost, SocialNetwork
+)
 
-# Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline, AnswerInline]
+
+
+admin.site.register(Quiz, QuizAdmin)
